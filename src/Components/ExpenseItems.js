c880e68413cf318d1db16773bb610e410ed806e2
Rahul Raj <rahulraj18670@gmail.com>
2024-02-18 01:15:20 +0530

    Created the files

---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,11 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <p>Food RS 10</p>
+      <p>Petrol RS 100</p>
+      <p>Movie RS200</p>
+    </div>
+  );
+}
+export default ExpenseItems;
