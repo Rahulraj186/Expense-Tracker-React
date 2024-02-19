5b5fa4e6e4f26615da3256be380218261fdad05a
Rahul Raj <rahulraj18670@gmail.com>
2024-02-19 14:21:44 +0530

    making seperate components

--- src/Components/ExpenseItems.js
+++ src/Components/ExpenseItems.js
@@ -1,15 +1,15 @@
 import "./ExpenseItems.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 function ExpenseItems(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title} </h2>
-        <div>
-          <h2>{props.location}</h2>
-        </div>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
     </div>
   );
 }
